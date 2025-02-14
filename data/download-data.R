e4550bafa7fa5bb76261b49ecd9807b4abe2d80c
Tarn van Eijk <t.vaneijk_1@tilburguniversity.edu>
2025-02-14 13:17:01 +0100

    Update of .R data download file, since errors occured for others

--- data/download-data.R
+++ data/download-data.R
@@ -8,19 +8,36 @@ library(tidyverse)
 library(googledrive)
 library(httpuv)
 
-# Downloading the files
-drive_download("Sampled_Data_Business.csv", overwrite = TRUE)
-drive_download("Sampled_Data_Review.csv", overwrite = TRUE)
-drive_download("Sampled_Data_Checkin.csv", overwrite = TRUE)
-drive_download("Sampled_Data_Tip.csv", overwrite = TRUE)
-drive_download("Data_User.csv", overwrite = TRUE)
+# Authenticate Google Drive (Users may need to sign in)
+drive_auth()
 
-# Loading the files in as csv
+# Define the Google Drive file IDs
+file_ids <- c(
+  "1GmZeV7raNZtKLdks1SBMzUgkV1FKBHOK", # Sampled_Data_Business.csv
+  "1MxG9RhUvjVSn00vYoeNEJE_cbS8wY6sh",  # Sampled_Data_Review.csv
+  "1VuFVuuL8wQOQkypmrmckbSNjQGxe1AlS",  # Sampled_Data_Checkin.csv
+  "1S2sY2n9QqXpM-X718KAiZqR4fYYlqWJb",  # Sampled_Data_Tip.csv
+  "1AduVWGiFWxQf9mpicRTeHmN35Y1CB9oh"   # Data_User.csv
+)
+
+# Define file names for saving
+file_names <- c(
+  "Sampled_Data_Business.csv",
+  "Sampled_Data_Review.csv",
+  "Sampled_Data_Checkin.csv",
+  "Sampled_Data_Tip.csv",
+  "Data_User.csv"
+)
+
+# Loop through the files and download them
+for (i in seq_along(file_ids)) {
+  drive_download(as_id(file_ids[i]), path = file_names[i], overwrite = TRUE)
+}
+
+# Load the downloaded files into R
 Sampled_Data_Business <- read_csv("Sampled_Data_Business.csv")
 Sampled_Data_Review <- read_csv("Sampled_Data_Review.csv")
 Sampled_Data_Checkin <- read_csv("Sampled_Data_Checkin.csv")
 Sampled_Data_Tip <- read_csv("Sampled_Data_Tip.csv")
 Data_User <- read_csv("Data_User.csv")
-
-
 
