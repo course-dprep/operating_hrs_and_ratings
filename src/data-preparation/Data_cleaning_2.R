87607abe66e7226c72a1bfee6660c795e66597f0
Isa Romeijn <i.romeijn@tilburguniversity.edu>
2025-02-14 09:57:55 +0100

    Update Data_cleaning_2.R in workflow of class
    First do data inspection, then data cleaning & transformation. 
    Used the order of the tutorial Introduction to R.

--- src/data-preparation/Data_cleaning_2.R
+++ src/data-preparation/Data_cleaning_2.R
@@ -1,12 +1,12 @@
+#Data Inspection 
+head(Data_Merged, 50)
+summary(Data_Merged)
+
 # Filtering for Restaurants that are open
 Data_Merged_Filtered <- Data_Merged %>% 
   filter(str_detect(categories, "Restaurants"),
-         is_open == 1 %>% 
-           select(review_count, name, state, categories, hours, business_id, text, stars.x, stars.y, user_id))
-         
-# renaming the star.x, star.y and text variables
-Data_Merged_Filtered <- Data_Merged_Filtered %>% 
-  rename(Stars_Business = stars.x, Stars_Users = stars.y, Review = text) 
+         is_open == 1 %>%)
+summary(Data_Merged_Filtered)
          
 #Inspection of NA's
 # Count number of NAs in each column and row 
@@ -16,5 +16,8 @@ rowSums(is.na(Data_Merged_Filtered))
 #Drop NA only in crucial columns 
 Data_Merged_Filtered <- Data_Merged_Filtered %>% drop_na(categories, hours, business_id, text, stars.x, stars.y)
 
-# Getting rid of the NA's
-Data_Merged_Filtered <- na.omit(Data_Merged_Filtered)
+#Data cleaning & Transformation
+#Dropping columns and renaming star.x, star.y and text variables 
+Data_Merged_Filtered_Cleaned <- Data_Merged_Filtered %>% select(review_count, name, state, categories, hours, business_id, text, stars.x, stars.y, user_id)
+Data_Merged_Filtered_Cleaned <- Data_Merged_Filtered_Cleaned %>% 
+  rename(Stars_Business = stars.x, Stars_Users = stars.y, Review = text) 
