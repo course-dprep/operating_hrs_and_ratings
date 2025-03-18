3a413b944cb00cad334dbb7a67dc9d9eff3fb105
iromeijn <i.romeijn@tilburguniversity.edu>
2025-03-18 13:53:47 +0100

    Remove unneccessary files in sample script and add relative paths

--- src/data-preparation/Sampling_Raw_Data.R
+++ src/data-preparation/Sampling_Raw_Data.R
@@ -8,9 +8,6 @@ library(dplyr)
 # Loading in the data sets
 Data_Reviews <- read_csv(here("yelp_academic_dataset_review.csv"))
 Data_Business <- read_csv(here("yelp_academic_dataset_business.csv"))
-Data_Checking <- read_csv(here("yelp_academic_dataset_checkin.csv"))
-Data_Tip <- read_csv(here("yelp_academic_dataset_tip.csv"))
-Data_User <- read_csv(here("yelp_academic_dataset_user.csv"))
 
 # Sample Business
 Sampled_Business <- Data_Business %>% 
@@ -35,8 +32,5 @@ Sampled_Checking <- Data_Checking %>%
   semi_join(Sampled_Business, by = "business_id")
 
 # Writing the csv's
-write_csv(Sampled_Business, "Sampled_Data_Business.csv")
-write_csv(Sampled_Review, "Sampled_Data_Review.csv")
-write_csv(Sampled_Tip, "Sampled_Data_Tip.csv")
-write_csv(Sampled_Checking, "Sampled_Data_Checkin.csv")
-
+write_csv(Sampled_Business, here("data", "Sampled_Data_Business.csv")
+write_csv(Sampled_Review, here("data","Sampled_Data_Review.csv")
