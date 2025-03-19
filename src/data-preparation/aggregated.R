60e904bb7109e0a0cc5d1ab20eaeb9a46e9fbcbf
iromeijn <i.romeijn@tilburguniversity.edu>
2025-03-19 22:02:11 +0100

    Create new rscript aggregated.R for second dataset for RQ1

---
+++ src/data-preparation/aggregated.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(here)
+
+# Load cleaned dataset
+Yelp_clean <- read_csv(here("gen", "output", "Yelp_clean.csv"))
+
+# Converting variables to factors
+Yelp_clean$Hours_category <- factor(Yelp_clean$Hours_category, levels = c("low", "middle", "high"))
+Yelp_clean$Stars_Category <- factor(Yelp_clean$Stars_Category, levels = c("low", "high")) # Changing the class of Stars_Category into a factor.
+Yelp_clean$state <- as.factor(Yelp_clean$state) 
+glimpse(Yelp_clean)
+
+# Aggregate data to ensure independent observations for RQ1 (one per restaurant)
+Yelp_clean_aggregated <- Yelp_clean %>%
+  group_by(business_id) %>%
+  summarise(
+    Stars_Category = first(Stars_Category),  
+    Hours_category = first(Hours_category),  
+    state = first(state),                    
+    review_count = first(review_count)       
+  ) %>%
+  ungroup()
+
+write_csv(Yelp_clean_aggregated, here("gen", "output", "Yelp_clean_aggregated.csv"))
